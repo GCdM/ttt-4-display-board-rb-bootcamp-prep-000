0404a9e17196ea7b98eccd950671a48911391d44
Gabriel Miranda <gabrielcm.br@gmail.com>
2018-05-14 16:47:49 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,8 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(board)
-  print
+  print " #{board[0]} "
+  print "| #{board[0]} |"
+  puts " #{board[0]} "
+  puts ""
 end
